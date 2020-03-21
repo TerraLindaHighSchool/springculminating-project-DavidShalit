17ad63a6e4ee19fc3e6fc4aaa91311b59bc42d73
DavidShalit <59098962+DavidShalit@users.noreply.github.com>
2020-03-20 17:34:32 -0700

    Completed Fire obstacle sequence

---
+++ Crashing Balls/Assets/Scripts/FireHitbox.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireHitbox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool ableToMove = true;
+    void Start()
+    {
+        InvokeRepeating("CycleCall", 0, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ableToMove)
+        {
+            transform.Translate(0, 0, 10 * Time.deltaTime);
+        }
+    }
+
+    void CycleCall()
+    {
+        StartCoroutine(Cycle());
+    }
+
+    IEnumerator Cycle()
+    {
+        yield return new WaitForSeconds(2.5f);
+        ableToMove = false;
+        yield return new WaitForSeconds(2.5f);
+        ableToMove = true;
+        yield return new WaitForSeconds(5);
+        transform.position = new Vector3(transform.position.x, transform.position.y, 31.97f);
+    }
+}
