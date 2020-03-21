171d896adf51877faf34c4be5021502f35eb95e0
DavidShalit <59098962+DavidShalit@users.noreply.github.com>
2020-03-20 17:20:22 -0700

    Added flames

---
+++ Crashing Balls/Assets/Scripts/ActiveTimer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveTimer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float interval = 5;
+    public ParticleSystem flame;
+    void Start()
+    {
+        flame = gameObject.GetComponent<ParticleSystem>();
+        InvokeRepeating("BruhMethodMoment", 0, interval * 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void BruhMethodMoment() //random name, its the method to call the IEnumerator
+    {
+        StartCoroutine(Timer());
+    }
+
+    IEnumerator Timer()
+    {
+        flame.Play();
+        yield return new WaitForSeconds(interval);
+        flame.Stop(false, ParticleSystemStopBehavior.StopEmitting);
+    }
+}
