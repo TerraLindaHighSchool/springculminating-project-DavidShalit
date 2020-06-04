960a5040fdcc5c773dd26ebcad115eeccf658e51
DavidShalit <59098962+DavidShalit@users.noreply.github.com>
2020-06-03 21:54:21 -0700

    Playable Prototype Complete

---
+++ Crashing Balls/Assets/Scripts/Turtle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turtle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isMoving;
+    void Start()
+    {
+        InvokeRepeating("Movement", 0, 20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)
+        {
+            transform.Translate(new Vector3(0, 0, -0.1f));
+        }
+    }
+     IEnumerator MoveCycle()
+    {
+        isMoving = true;
+        yield return new WaitForSeconds(10);
+        isMoving = false;
+    }
+
+    void Movement()
+    {
+        StartCoroutine(MoveCycle());
+        
+        transform.Rotate(Vector3.up * 180);
+    }
+}
